dbd0db279dfa6f1c3385652302a2a66fc4ca97f1
dinesh99999 <bdinesh99999@gmail.com>
2020-11-09 07:09:59 +0530

    Adding one way binding

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector:'pm-root',
-  template:`
-    <div>
-      <h1>{{title}}</h1>
-    </div>
-  `
+  templateUrl : './app.component.html'
 })
 export class AppComponent{
   title: string = "This is Dinesh Project";
